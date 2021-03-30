893afe16baaa231ae54c4be27b9514db40101245
jordanlahipramelb <jordan.lahipramelb@gmail.com>
2021-03-29 17:00:19 -0700

    first commit

---
+++ exercise.js
@@ -0,0 +1,18 @@
+// var PI = 3.14;
+// PI = 42; // stop me from doing this!
+
+// const PI = 3.14
+// PI = 42 //Error!!! Can't reassign!
+
+// let and const Exercises
+
+// What is the difference between var and let?
+// 	var is able to be redeclared and is also function scoped.
+// 	let can't be redeclared and is block scoped.
+// What is the difference between var and const?
+// 	const cannot be redeclared or reassigned.
+// 	const is also block scoped.
+// What is the difference between let and const?
+// 	let can be reassigned.
+// What is hoisting?
+//  variable hoisting is when a variable declaration is moved to the top of their scope before their execution.
